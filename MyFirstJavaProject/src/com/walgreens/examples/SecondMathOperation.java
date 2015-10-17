94ebeddce9ce921a5cd2dc2b22e8d5ca5ad90fb5
sunilpatel72001 <sunil_patel72001@yahoo.com>
2015-10-17 12:02:47 -0500

    First Changes in file
    File Name and code changes

---
+++ MyFirstJavaProject/src/com/walgreens/examples/SecondMathOperation.java
@@ -0,0 +1,21 @@
+package com.walgreens.examples;
+
+public class SecondMathOperation {
+
+	public static void main(String[] args){
+		
+			addition();
+	}
+	
+	  public static void addition(){
+		 //  syntex; datatype variable = value
+	     // example of datatype; int double;
+	        int number1 =10;
+	        int number2 = 20;
+	        int number3 = 10;
+	        
+	        int result = number1 + number2 + number3;
+	       System.out.println("Addition is : "+result);
+	       
+	   }
+}
---
+++ MyFirstJavaProject/src/com/walgreens/examples/ThirdMathOperation.java
@@ -0,0 +1,25 @@
+package com.walgreens.examples;
+
+public class ThirdMathOperation {
+
+	public static  void main (String[]args){
+		
+		addSub();
+		
+	}
+	public static  void addSub(){
+	
+	//Syntex :  Datatype variable = value
+	
+	//Example of datatype:  int, double
+	
+		int number1 = 20;
+		int number2 = 23;
+		int number3 = 32;
+		
+		int result = number1 + number2 - number3;
+		System.out.println("Answer is : "+ result);
+		
+	}
+	
+	}
